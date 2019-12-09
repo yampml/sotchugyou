16b71118528e7dcb41db8c1271960e547badfe4d
yampml <nguyendinhan97@gmail.com>
2019-12-09 15:30:49 +0700

    done contract 1

--- chaincode/bkeca/data.go
+++ chaincode/bkeca/data.go
@@ -73,6 +73,63 @@ type StudentCertificate struct {
 	IssuedBy    string        `json:"issued_by"`
 }
 
+// Init callback representing the invocation of a chaincode
+func (t *SmartContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+// Invoke Function accept blockchain code invocations.
+func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	// Retrieve the requested Smart Contract function and arguments
+	function, args := stub.GetFunctionAndParameters()
+	fmt.Println(function)
+	fmt.Println(args)
+	// Route to the appropriate handler function to interact with the ledger appropriately
+	if function == "initStudent" {
+		return t.initStudent(stub)
+	} else if function == "queryStudentInfo" {
+		return t.queryStudentInfo(stub, args)
+	} else if function == "createStudentInfo" {
+		return t.createStudentInfo(stub, args)
+	} else if function == "updateStudentInfo" {
+		return t.updateStudentInfo(stub, args)
+	} else if function == "deleteStudentInfo" {
+		return t.deleteStudentInfo(stub, args)
+	} else if function == "queryStudentByEmail" {
+		return t.queryStudentByEmail(stub, args)
+	} else if function == "initStudentTestInfos" {
+		return t.initStudentTestInfos(stub)
+	} else if function == "queryStudentTestInfo" {
+		return t.queryStudentTestInfo(stub, args)
+	} else if function == "createStudentTestInfo" {
+		return t.createStudentTestInfo(stub, args)
+	} else if function == "createStudentTestInfo" {
+		return t.deleteStudentTestInfo(stub, args)
+	} else if function == "initClassroom" {
+		return t.initClassroom(stub)
+	} else if function == "queryClassroomInfo" {
+		return t.queryClassroomInfo(stub, args)
+	} else if function == "createClassroomInfo" {
+		return t.createClassroomInfo(stub, args)
+	} else if function == "updateClassroomInfo" {
+		return t.updateClassroomInfo(stub, args)
+	} else if function == "deleteClassroomInfo" {
+		return t.deleteStudentInfo(stub, args)
+	} else if function == "initQuestionInfo" {
+		return t.initQuestionInfo(stub)
+	} else if function == "queryQuestionInfo" {
+		return t.queryQuestionInfo(stub, args)
+	} else if function == "createQuestionInfo" {
+		return t.createQuestionInfo(stub, args)
+		// } else if function == "updateClassroomInfo" {
+		// 	return t.updateClassroomInfo(stub, args)
+	} else if function == "deleteQuestionInfo" {
+		return t.deleteQuestionInfo(stub, args)
+	}
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
 func (t *SmartContract) initStudentCertificate(stub shim.ChaincodeStubInterface) pb.Response {
 	StudentCertificates := []StudentCertificate{
 		StudentCertificate{
@@ -169,62 +226,134 @@ func (t *SmartContract) queryStudentTestInfo(stub shim.ChaincodeStubInterface, a
 	}
 
 	studentTestInfoAsBytes, _ := stub.GetState("STI" + args[0])
-	fmt.Println(studentTestInfoAsBytes)
 	return shim.Success(studentTestInfoAsBytes)
 }
 
-// Init callback representing the invocation of a chaincode
-func (t *SmartContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
+//peer chaincode invoke -n bkecacc -c '{"Args":["createStudentTestInfo","2","{\"student\":{\"student_id\":\"1\",\"username\":\"Nguyen Dinh An\",\"email\":\"nguyendinhan97@gmail.com\",\"dob\":\"08/07/1997\"},\"classroom\":{\"classroom_id\":\"1\",\"name\":\"Computing Class 1\"},\"question_responses\":[{\"question\":{\"question_id\":\"1\",\"description\":\"Why is C++ so fast?\",\"right_choice\":\"1\",\"choices\":[\"Choice 1\",\"Choice 2\",\"Choice 3\",\"Choice 4\"]},\"student_choices\":[\"1\"]},{\"question\":{\"question_id\":\"2\",\"description\":\"Why is Java so slow?\",\"right_choice\":\"1\",\"choices\":[\"Choice 1\",\"Choice 2\",\"Choice 3\",\"Choice 4\"]},\"student_choices\":[\"1\",\"1\"]}],\"mark\":\"2/2\",\"passed\":true,\"started_at\":\"0001-01-01T00:00:00Z\",\"finished_at\":\"2009-11-10T23:00:00Z\"}"]}' -C myc
+
+func (t *SmartContract) createStudentTestInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 {
+		return shim.Error("Incorrect number of arguments. Expecting 2")
+	}
+
+	fmt.Println(args[1]) // StudentInfo
+
+	studentTestInfo := StudentTestInfo{}
+	json.Unmarshal([]byte(args[1]), &studentTestInfo)
+
+	studentTestInfoAsBytes, _ := json.Marshal(studentTestInfo)
+	stub.PutState("STI"+args[0], studentTestInfoAsBytes)
+
+	// indexName := "email~usrid"
+	// emailUseridIndexKey, err := stub.CreateCompositeKey(indexName, []string{args[2], "USR" + args[0]})
+	// if err != nil {
+	// 	return shim.Error(err.Error())
+	// }
+	// value := []byte{0x00}
+	// stub.PutState(emailUseridIndexKey, value)
+
 	return shim.Success(nil)
 }
 
-// Invoke Function accept blockchain code invocations.
-func (t *SmartContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	// Retrieve the requested Smart Contract function and arguments
-	function, args := stub.GetFunctionAndParameters()
-	fmt.Println(function)
-	fmt.Println(args)
-	// Route to the appropriate handler function to interact with the ledger appropriately
-	if function == "initStudent" {
-		return t.initStudent(stub)
-	} else if function == "queryStudentInfo" {
-		return t.queryStudentInfo(stub, args)
-	} else if function == "createStudentInfo" {
-		return t.createStudentInfo(stub, args)
-	} else if function == "updateStudentInfo" {
-		return t.updateStudentInfo(stub, args)
-	} else if function == "deleteStudentInfo" {
-		return t.deleteStudentInfo(stub, args)
-	} else if function == "queryStudentByEmail" {
-		return t.queryStudentByEmail(stub, args)
-	} else if function == "initStudentTestInfos" {
-		return t.initStudentTestInfos(stub)
-	} else if function == "queryStudentTestInfo" {
-		return t.queryStudentTestInfo(stub, args)
-	} else if function == "initClassroom" {
-		return t.initClassroom(stub)
-	} else if function == "queryClassroomInfo" {
-		return t.queryClassroomInfo(stub, args)
-	} else if function == "createClassroomInfo" {
-		return t.createClassroomInfo(stub, args)
-	} else if function == "updateClassroomInfo" {
-		return t.updateClassroomInfo(stub, args)
-	} else if function == "deleteClassroomInfo" {
-		return t.deleteStudentInfo(stub, args)
-	} else if function == "initQuestionInfo" {
-		return t.initQuestionInfo(stub)
-	} else if function == "queryQuestionInfo" {
-		return t.queryQuestionInfo(stub, args)
-	} else if function == "createQuestionInfo" {
-		return t.createQuestionInfo(stub, args)
-		// } else if function == "updateClassroomInfo" {
-		// 	return t.updateClassroomInfo(stub, args)
-	} else if function == "deleteQuestionInfo" {
-		return t.deleteQuestionInfo(stub, args)
+// //peer chaincode invoke -n bkecacc -c '{"Args":["updateStudentInfo", "1", "HieuFoobar", "updated@mail.com","01-01-1991"]}' -C myc
+// func (t *SmartContract) updateStudentInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+// 	if len(args) != 4 {
+// 		return shim.Error("Incorrect number of arguments. Expecting 4")
+// 	}
+
+// 	usrAsBytes, _ := stub.GetState("USR" + args[0])
+// 	usr := StudentInfo{}
+// 	json.Unmarshal(usrAsBytes, &usr)
+
+// 	usr.StudentID = args[0]
+// 	usr.Username = args[1]
+// 	usr.Email = args[2]
+// 	usr.Dob = args[3]
+
+// 	usrAsBytes, _ = json.Marshal(usr)
+// 	stub.PutState("USR"+args[0], usrAsBytes)
+
+// 	return shim.Success(nil)
+// }
+
+// peer chaincode invoke -n bkecacc -c '{"Args":["deleteUserInfo", "1"]}' -C myc
+func (t *SmartContract) deleteStudentTestInfo(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	return shim.Error("Invalid Smart Contract function name.")
+	err := stub.DelState("STI" + args[0])
+	if err != nil {
+		return shim.Error(fmt.Sprintf("Failed to delete state with key: %s", args[0]))
+	}
+
+	// indexName := "email~usrid"
+	// emailUseridIndexKey, err2 := stub.CreateCompositeKey(indexName, []string{args[1], "USR" + args[0]})
+	// if err2 != nil {
+	// 	return shim.Error(err2.Error())
+	// }
+	// stub.DelState(emailUseridIndexKey)
+
+	return shim.Success(nil)
 }
+
+// // peer chaincode invoke -n bkecacc -c '{"Args":["queryUserByEmail", "ndahht@gmail.com"]}' -C myc
+// func (t *SmartContract) queryStudentByEmail(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+// 	if len(args) != 1 {
+// 		return shim.Error("Incorrect number of arguments. Expecting 1")
+// 	}
+
+// 	email := args[0]
+// 	indexName := "email~usrid"
+
+// 	resultsIterator, err := stub.GetStateByPartialCompositeKey(indexName, []string{email})
+// 	if err != nil {
+// 		return shim.Error(err.Error())
+// 	}
+// 	defer resultsIterator.Close()
+
+// 	var buffer bytes.Buffer
+// 	buffer.WriteString("[")
+// 	bArrayMemberAlreadyWritten := false
+
+// 	var i int
+// 	for i = 0; resultsIterator.HasNext(); i++ {
+// 		responseRange, err := resultsIterator.Next()
+// 		if err != nil {
+// 			return shim.Error(err.Error())
+// 		}
+
+// 		_, compositeKeyParts, err := stub.SplitCompositeKey(responseRange.Key)
+// 		if err != nil {
+// 			return shim.Error(err.Error())
+// 		}
+
+// 		returnedStudentID := compositeKeyParts[1]
+
+// 		usrAsBytes, _ := stub.GetState(returnedStudentID)
+
+// 		// Add a comma before array members, suppress it for the first array member
+// 		if bArrayMemberAlreadyWritten == true {
+// 			buffer.WriteString(",")
+// 		}
+// 		buffer.WriteString("{\"Key\":")
+// 		buffer.WriteString("\"")
+// 		buffer.WriteString(returnedStudentID)
+// 		buffer.WriteString("\"")
+
+// 		buffer.WriteString(", \"Record\":")
+// 		// Record is a JSON object, so we write as-is
+// 		buffer.WriteString(string(usrAsBytes))
+// 		buffer.WriteString("}")
+// 		bArrayMemberAlreadyWritten = true
+// 	}
+// 	buffer.WriteString("]")
+
+// 	fmt.Printf("- queryStudentByEmail:\n%s\n", buffer.String())
+
+// 	return shim.Success(buffer.Bytes())
+
+// }
 
 // peer chaincode invoke -n bkecacc -c '{"Args":["initClassroom"]}' -C myc
 func (t *SmartContract) initClassroom(stub shim.ChaincodeStubInterface) pb.Response {
